a7c0c29e820aec5445c0c33d0ca1373e76495d5b
canekmekciler <can.ekmekciler@gmail.com>
2022-11-01 16:23:34 +0300

    Added filtering and styling

--- src/components/Contacts/List/index.js
+++ src/components/Contacts/List/index.js
@@ -1,7 +1,35 @@
-import React from "react";
+import React, {useState} from "react";
 
-function List() {
-    return <div>List</div>;
+function List({ contacts }) {
+    const [filterText, setFilterText] = useState('');
+
+    const filtered = contacts.filter((item) => {
+        return Object.keys(item).some((key) =>
+            item[key]
+                .toString()
+                .toLowerCase()
+                .includes(filterText.toLocaleLowerCase())
+        );
+    });
+
+    return (
+        <div>
+            <input
+                placeholder="Filter Contact"
+                value={filterText}
+                onChange={(event) => setFilterText(event.target.value)}
+            />
+            <ul className="List">
+                {filtered.map((contact, index) => (
+                    <li key={index}>
+                        <span>{contact.fullName}</span>
+                        <span>{contact.phone_number}</span>
+                    </li>
+                ))}
+            </ul>
+            <p>Total Contacts Number ({filtered.length})</p>
+        </div>
+    );
 }
 
 export default List;
--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -2,17 +2,32 @@ import React from 'react'
 import List from './List'
 import Form from './Form'
 import { useState, useEffect } from 'react'
+import './style.css'
 
 function Contacts() {
-    const [contacts, setContacts] = useState([]);
+    const [contacts, setContacts] = useState([
+        {
+            fullName: 'Can',
+            phone_number: '123456'
+        },
+        {
+            fullName: 'Jack',
+            phone_number:'654321'
+        },
+        {
+            fullName: 'Danny',
+            phone_number:'984213'
+        }
+    ]);
 
     useEffect(() => {
         console.log(contacts)
     }, [contacts])
 
     return (
-        <div>
-            <List />
+        <div className='container'>
+            <h1>Contacts App</h1>
+            <List contacts={contacts} />
             <Form addContact={setContacts} contacts={contacts} />
         </div>
     )
